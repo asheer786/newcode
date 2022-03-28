e229657b41342683ae742d3478a9afa14015d852
asheer786 <81909047+asheer786@users.noreply.github.com>
2022-03-28 15:51:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          welcome to my world
+          welcome
         </a>
       </header>
     </div>
